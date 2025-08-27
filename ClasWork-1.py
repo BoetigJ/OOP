78059c3bd6f95d75ed1abaabe3db0f7a0512cb92
BoetigJ <jacobboetig@gmail.com>
2025-08-27 13:52:08 -0500

    ClassWork-1

---
+++ ClasWork-1.py
@@ -0,0 +1,12 @@
+student_name = input("enter the student name: ")
+course1 = int(input("emter the grade for course1: "))
+course2 = int(input("emter the grade for course2: "))
+course3 = int(input("emter the grade for course3: "))
+course4 = int(input("emter the grade for course4: "))
+course5 = int(input("emter the grade for course5: "))
+
+total = (course1+course2+course3+course4+course5)/5
+print("Your total points are:", total, "%")
+
+
+
